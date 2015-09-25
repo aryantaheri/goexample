a5c56fbb77ee60b01e00d28a626c694406fc32b7
Aryan <aryantaheri@gmail.com>
2015-09-25 21:54:57 +0200

    Some update to hello

---
+++ hello/flowcontrol.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func mainx(){
+	fmt.Println("flowcontrol")
+}
--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/aryantaheri/stringutil"
+	"github.com/aryantaheri/goexample/stringutil"
 	"math/rand"
 	"math"
 	"math/cmplx"
+	"runtime"
 	)
-
 
 func add(x int, y int) int {
 	return x + y	
@@ -61,6 +61,72 @@ func inference(){
 	fmt.Printf("Type of %v is %T", v, v)
 	fmt.Printf("Type of %v is %T", x, x)}
 
+func myfor(){
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	} 
+	fmt.Printf("sum is %v\n", sum)
+}
+
+func myfor2(){
+	sum := 1
+	for ; sum < 10;  {
+		sum += sum
+	} 
+	fmt.Printf("sum is %v\n", sum)
+}
+
+func ifsqrt(x float64) string {
+	if x < 0 {
+		return ifsqrt(-x) + "i"
+	}	
+	return fmt.Sprint(math.Sqrt(x)) 
+	}
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func Sqrt(x float64) float64 {
+	z := float64(1)
+	return z
+}
+
+func myswitch() {
+	switch os := runtime.GOOS; os {
+		case "linux":
+			fmt.Printf("OS is Linux ", os)
+		default:
+			fmt.Printf("%s.", os)
+	}
+}
+
+func mydefer(){
+	i := 0
+	defer fmt.Println("world", i)
+	i++
+	defer fmt.Println("xxx", i)
+	i++
+	fmt.Println("hello", i)
+}
+
+func pointers(){
+	i, j := 42, 2701
+	p := &i         // point to i
+	fmt.Println("i through pointer", *p) // read i through the pointer
+	fmt.Println("i's  pointer", p)
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
+
+}
 func main() {
 	
 	fmt.Printf("Hello Aryan.\n")
@@ -90,5 +156,23 @@ func main() {
 	
 	fmt.Println("inferencefunc:")
 	inference()
+	
+	fmt.Println("\nmyfor:")
+	myfor()
+	myfor2()
+	
+	fmt.Println("\nifsqrt:", ifsqrt(-2), ifsqrt(-16))
+	
+	fmt.Println("\npow:", pow(3, 2, 10), pow(3, 3, 20))
+	
+	fmt.Println("\nmyswitch:")
+	myswitch()
+	
+	fmt.Println("\nmydefer:")
+	mydefer()
+	
+	fmt.Println("\npointers:")
+	pointers()
+	
 }
 
