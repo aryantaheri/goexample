f545b36c1548b465fe65bd8a95036daa7ada4154
Aryan <aryantaheri@gmail.com>
2015-09-27 22:41:08 +0200

    Crawler

--- crawler/gocrawler.go
+++ crawler/gocrawler.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"sort"
+	"net/http"
+	"io/ioutil"
+	"io"
+	"golang.org/x/net/html"
 )
 
 type Fetcher interface {
@@ -32,9 +38,20 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 }
 
 func main() {
-	Crawl("http://golang.org/", 4, fetcher)
+	words.wordList = make([]string, 100)
+	words.wordMap = make(map[string]int)
+	Crawl("http://wiki.ux.uis.no/foswiki/Info/WebHome", 4, fetcher)
+	sort.Sort(sort.Reverse(&words))
+	printWords(words)
 }
 
+func printWords(words Words) {
+	for i, word := range words.wordList {
+		if word != "" {
+			fmt.Printf("Rank:%v  %v #%v\n", i, word, words.wordMap[word])
+		}
+	}
+}
 // fakeFetcher is Fetcher that returns canned results.
 type fakeFetcher map[string]*fakeResult
 
@@ -44,12 +61,91 @@ type fakeResult struct {
 }
 
 func (f fakeFetcher) Fetch(url string) (string, []string, error) {
-	if res, ok := f[url]; ok {
-		return res.body, res.urls, nil
+	response, err := http.Get(url)
+	fmt.Printf("Fetch	URL:%v\n	Reponse: %v\n	Err: %v\n", url, response, err)
+	if err != nil {
+		// handle error
 	}
-	return "", nil, fmt.Errorf("not found: %s", url)
+//	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	fmt.Printf("Body:\n	%q\n	%v\n", body, err)
+	
+//	wordCount(string(body))
+	
+	urls := getUrls(response.Body)
+	fmt.Printf("URLS:\n%v\n", urls)
+	return string(body), urls, nil
+//	if res, ok := f[url]; ok {
+//		return res.body, res.urls, nil
+//	}
+//	return "", nil, fmt.Errorf("not found: %s", url)
 }
 
+func getUrls(body io.ReadCloser) []string {
+	page := html.NewTokenizer(body)
+	
+	urls := make([]string, 0)
+	fmt.Println("getUrls")
+	for {
+		tokenType := page.Next()
+		fmt.Println("getUrls, tokenType: ", tokenType)
+		switch tokenType {
+			case html.TextToken:
+				wordCount(string(page.Text()))
+			case html.StartTagToken:
+				token := page.Token()
+				if token.DataAtom.String() == "a" {
+					for _, attr := range token.Attr {
+						if attr.Key == "href" {
+							urls = append(urls, attr.Val)
+						}
+					}
+				}
+			case html.ErrorToken:
+				fmt.Println("ErrorToken: ", page.Err())
+				return urls
+				
+		}
+	}
+	return urls
+}	
+
+type Words struct {
+	wordMap map[string]int
+	wordList []string
+}
+
+var words Words
+
+func (words *Words) Len() int {
+	return len(words.wordList)
+}
+
+func (words *Words) Less(i, j int) bool {
+	return words.wordMap[words.wordList[i]] < words.wordMap[words.wordList[j]]
+} 
+
+func (words *Words) Swap(i, j int)  {
+	words.wordList[i], words.wordList[j] = words.wordList[j], words.wordList[i] 
+} 
+
+func wordCount(body string) {
+	fields := strings.Fields(body)
+	fmt.Println("wordCount")
+	for i, field := range fields {
+		fmt.Printf("	field %d %s\n", i, field)
+		word := cleanWord(field)
+		if _, exist := words.wordMap[word]; !exist {
+			words.wordList = append(words.wordList, word)
+		}
+		words.wordMap[word] += 1 
+	}
+}
+
+func cleanWord(word string) string {
+	word = strings.Trim(word, " ~!@#$%^&*()_+}{|\":?></.,][';=-0987654321`\\")
+	return strings.ToLower(word)
+}
 // fetcher is a populated fakeFetcher.
 var fetcher = fakeFetcher{
 	"http://golang.org/": &fakeResult{
--- web/webserver.go
+++ web/webserver.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"io"
-	"net/http"
 	"fmt"
+	"golang.org/x/net/html"
+	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"time"
 )
 
 type Hello struct {
-	
 }
 
 func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "hello from ServeHTTP")
 }
 
-
 type WebStruct struct {
 	Greeting string
-	Punct string
-	Who string
+	Punct    string
+	Who      string
 }
 
 func (web WebStruct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -35,7 +35,6 @@ func (web WebStruct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-
 func serveHTTPTester() {
 	http.Handle("/greeting", &WebStruct{Greeting: "Hello there, greeting", Punct: "Punct?"})
 	http.Handle("/punct", WebStruct{Punct: "Hello there, Punct"})
@@ -47,23 +46,90 @@ func serveHTTPTester() {
 	}
 }
 
-
 func webHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, r.RemoteAddr + r.Host)
+	io.WriteString(w, r.RemoteAddr+r.Host)
 }
 
 func webHandlerTester() {
 	http.HandleFunc("/x", webHandler)
 	http.ListenAndServe(":8000", nil)
-	
+
 }
 
+func fetcher() {
+//	url := "http://wiki.ux.uis.no/foswiki/Info/WebHome"
+	url := "http://www.google.com"
+	response, err := http.Get(url)
+	fmt.Printf("Fetch	URL:%v\n	Reponse: %v\n	Err: %v\n", url, response, err)
 
+	body, err := ioutil.ReadAll(response.Body)
+	fmt.Printf("Body:	%q\nError:	%v\n", body, err)
+
+	doc, err := html.Parse(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		fmt.Println("helloooooo1", n)
+		
+		switch n.Type {
+			case html.TextNode:
+				fmt.Println("NodeType: TextNode ", n.Type) 
+			case html.DocumentNode:
+				fmt.Println("NodeType: DocumentNode ", n.Type) 
+			case html.ElementNode:
+				fmt.Println("NodeType: ElementNode ", n.Type, n.Data, n.DataAtom, n.Namespace, n.Attr) 
+			case html.CommentNode:
+				fmt.Println("NodeType: CommentNode ", n.Type) 
+			case html.DoctypeNode:
+				fmt.Println("NodeType: DoctypeNode ", n.Type)
+			case html.ErrorNode:
+				fmt.Println("NodeType: ErrorNode ", n.Type) 
+		}
+		
+		if n.Type == html.TextNode  {
+//			for _, a := range n.Attr {
+//				if a.Key == "href" {
+					fmt.Printf("	helloooooo2 %T %v\n", n, n)
+//					break
+//				}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+
+//	tokenizer := html.NewTokenizer(response.Body)
+//	fmt.Println("Tokenizer: ", tokenizer)
+//
+//	for {
+//		tokenType := tokenizer.Next()
+//		token := tokenizer.Token()
+//		fmt.Println("	tokenType: ", tokenType)
+//		fmt.Println("	token: ", token)
+//		switch tokenType {
+//		case html.ErrorToken:
+//			fmt.Println("	ErrorToken: ", tokenizer.Err())
+//			return
+//		case html.TextToken:
+//			fmt.Println("	TextToken: ", tokenizer.Text())
+//		case html.StartTagToken:
+//			tagName, _ := tokenizer.TagName()
+//			fmt.Println("	StartTagToken: ", tagName)
+//		default:
+//
+//		}
+//	}
+}
 
 func main() {
-//	fmt.Println("webHandlerTester")
-//	webHandlerTester()
-	
-	fmt.Println("serveHTTPTester")
-	serveHTTPTester()
+	//	fmt.Println("webHandlerTester")
+	//	webHandlerTester()
+
+	//	fmt.Println("serveHTTPTester")
+	//	serveHTTPTester()
+	fetcher()
 }
